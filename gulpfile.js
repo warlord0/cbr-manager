1a0d46d55f4bc75e45a13244eca806f4b839e19d
Warlord <bill_bixby@mail.com>
2016-12-11 15:42:35 +0000

    Resizable splitter
    Made the tree and covers operate as a resizeable split pane and save the
    position of the split in the config.

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,16 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
+var coffee = require('gulp-coffee');
+var sourcemaps = require('gulp-sourcemaps');
+var header = require('gulp-header');
 
 // Lint Task
 gulp.task('lint', function() {
     return gulp.src('js/*.js')
-        .pipe(jshint({
-            laxcomma: true
-        }))
+        // .pipe(jshint({
+        //     laxcomma: true
+        // }))
         .pipe(jshint.reporter('default'));
 });
 
@@ -58,11 +61,46 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('js'));
 });
 
+gulp.task('coffee', () => {
+    // using data from package.json
+    var pkg = require('./package.json');
+    var banner = ['###',
+      ' * <%= pkg.name %> - <%= pkg.description %>',
+      ' * @version v<%= pkg.version %>',
+      ' * @link <%= pkg.homepage %>',
+      ' * @license <%= pkg.license %>',
+      ' ###',
+      ''].join('\n');
+
+    return gulp.src([
+            'src/*.coffee',
+        ])
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(sourcemaps.init())
+        .pipe(coffee({
+            bare: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('.'))
+    &&
+    gulp.src([
+            'src/js/*.coffee'
+        ])
+        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(sourcemaps.init())
+        .pipe(coffee({
+            bare: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('js'));
+});
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch(['src/*.coffee', 'src/js/*.coffee'], ['coffee']);
+    // gulp.watch('js/*.js', ['lint', 'scripts']);
+    // gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'less', 'scripts']);
+gulp.task('default', ['coffee']); // ['lint', 'less', 'coffee', 'scripts']);
